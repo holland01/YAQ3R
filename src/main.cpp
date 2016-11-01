852f90bcc2fd71e5b9409d255e27f4d38fa33b5a
holland schutte <hgschutte1@gmail.com>
2016-10-31 23:58:03 -0700

    remove debug cruft and add return 0 for completeness

--- src/main.cpp
+++ src/main.cpp
@@ -46,19 +46,7 @@ static INLINE std::string FullPath( const char* path, size_t pathLen )
 	strPath.append( path, pathLen );
 	root.append( strPath );
 
-	//printf( "Path Received: %s\n", root.c_str() );
-
 	return root;
-}
-
-static void TryAppend( const char* base )
-{
-	std::string b( FullPath( base, strlen( base ) + 1 ) );
-	std::string ext( ".jpg" );
-
-	b.append( ext );
-
-	std::cout << "New Path: " << b << std::endl;
 }
 
 int main( void )
@@ -67,17 +55,6 @@ int main( void )
 	static_assert( sizeof( glm::vec2 ) == sizeof( float ) * 2, SIZE_ERROR_MESSAGE );
 	static_assert( sizeof( glm::ivec3 ) == sizeof( int ) * 3, SIZE_ERROR_MESSAGE );
 
-	TryAppend( "asset/models/mapobjects/Skull/skull" );
-
-	//std::string testPath("/working/asset/textures/gothic_door/"
-		//"door02_i_ornate5_fin");
-
-	//std::string accum( ".jpg" );
-
-	//testPath += accum;
-
-	//std::cout << "New Path: " << testPath << std::endl;
-
 #if defined( EMSCRIPTEN ) && defined( IOTEST )
 	IOTestWebWorker test;
 	return test();
@@ -85,4 +62,6 @@ int main( void )
 	gAppTest = new TRenderer( ASSET_Q3_ROOT"/maps/q3dm2.bsp" );
 	gAppTest->Load();
 #endif
+
+	return 0;
 }
