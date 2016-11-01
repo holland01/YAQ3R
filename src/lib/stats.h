881763741c9a84b9ec189854e3d0a718234bb208
holland schutte <hgschutte1@gmail.com>
2016-11-01 00:06:35 -0700

    simplified InsertOrderedUnique, provided better documentation, and also made Deviation()'s target parameter a Treal instead of Tnum since...why use integer computations?

--- src/lib/stats.h
+++ src/lib/stats.h
@@ -14,13 +14,17 @@ struct stats_t
 	// vector is the most versatile data structure for our purposes here:
 	// we can write code to ensure that its values are a) ordered and b)
 	// unique, while still maintaining the ability to do killer things
-	// like have read O(1) access and choose our insertion points
+	// like have O(1) read access and choose our insertion points
 	std::vector< Tnum > store;
 
 	bool InsertOrderedUnique( Tnum v, Tnum* slot = nullptr )
 	{
 		if ( store.empty() )
 		{
+			if ( slot )
+			{
+				*slot = 0;
+			}
 			store.push_back( v );
 			return true;
 		}
@@ -38,22 +42,22 @@ struct stats_t
 			*slot = i;
 		}
 
-		// vector::insert will insert a value
-		// before the specified index.
-		if ( k > v )
+		// Since store's values are ordered, and i increments
+		// as long as (k = store[ i ]) < v, we can be certain that
+		// if v does exist in store, k = v here.
+		if ( k == v )
 		{
-			store.insert( store.begin() + i, v );
-			return true;
+			return false;
 		}
-		else if ( k < v ) // i == store.size(), so we append
-		{
-			store.push_back( v );
-			return true;
-		}
-		// else k == v, and we don't want to insert it
-		// since we want unique only...
 
-		return false;
+		// In the case that v < store[ 0 ],
+		// v is inserted into the front of the list.
+		// If v > all k, store.begin() + i = store.begin() + store.size()
+		// = store.end(), so we just append it as normal.
+		// Otherwise, since vector::insert always inserts before begin() + i,
+		// and k_i > v, v will be inserted right before k_i.
+		store.insert( store.begin() + i, v );
+		return true;
 	}
 
 	void InsertOrdered( Tnum v )
@@ -71,14 +75,19 @@ struct stats_t
 		return Treal( Sum() ) / Treal( store.size() );
 	}
 
-	Treal Deviation( Tnum target ) const
+	Treal Deviation( Treal target ) const
 	{
 		Treal sum = 0;
 		for ( uint16_t i = 0; i < store.size(); ++i )
 		{
-			sum += std::pow( Treal( store[ i ] - target ), Treal( 2 ) );
+			sum += std::pow( Treal( store[ i ] ) - target, Treal( 2 ) );
 		}
-		return std::sqrt( sum / Treal( target ) );
+		return std::sqrt( sum / target );
+	}
+
+	Treal Deviation( void ) const
+	{
+		return Deviation( Average() );
 	}
 
 	Treal ZScore( Tnum target, Treal average, Treal deviation ) const
@@ -127,4 +136,3 @@ struct stats_t
 		return store[ store.size() - 1 ];
 	}
 };
-
