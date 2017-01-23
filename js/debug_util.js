7e482072e45caa9b77c4fe8852b5fb589bc81796
holland schutte <hgschutte1@gmail.com>
2017-01-23 01:10:52 -0800

    minor comment correction

--- js/debug_util.js
+++ js/debug_util.js
@@ -69,7 +69,7 @@ var getHexString = function(str) {
 
 var buildStringFromU8 = function(array, start, end) {
 	if (!end) {
-		end = Number.MAX_SAFE_INTEGER >> 32; // so we don't wait for an eternity
+		end = Number.MAX_SAFE_INTEGER >> 32; // so we don't potentially wait for an eternity
 	}
 	let str = "";
 	for (let i = start; array[i] !== 0 && i < array.length && i < end; ++i) {
