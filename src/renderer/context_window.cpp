59f861c0f0ef2398c704ae9b0f5f24c11fa5b074
holland schutte <hgschutte1@gmail.com>
2017-08-08 01:20:51 -0700

    added a context info printing function.

--- src/renderer/context_window.cpp
+++ src/renderer/context_window.cpp
@@ -1,17 +1,48 @@
 #include "context_window.h"
 #include "renderer_local.h"
 #include "io.h"
+#include "glutil.h"
 
 extern "C" {
+
+void GPrintContextInfo( void )
+{
+	int depthSize;
+	int redSize, greenSize, blueSize, alphaSize;
+
+	SDL_GL_GetAttribute( SDL_GL_DEPTH_SIZE, &depthSize );
+	SDL_GL_GetAttribute( SDL_GL_RED_SIZE, &redSize );
+	SDL_GL_GetAttribute( SDL_GL_GREEN_SIZE, &greenSize );
+	SDL_GL_GetAttribute( SDL_GL_BLUE_SIZE, &blueSize );
+	SDL_GL_GetAttribute( SDL_GL_ALPHA_SIZE, &alphaSize );
+
+	std::stringstream out;
+	out << "Buffer Sizes\n"
+		<< "\tDepth: " << depthSize << "\n"
+		<< "\tRed: " << redSize << "\n"
+		<< "\tGreen: " << greenSize << "\n"
+		<< "\tBlue: " << blueSize << "\n"
+		<< "\tAlpha: " << alphaSize << "\n";
+
+	MLOG_INFO( "OpenGL Context Info, %s", out.str().c_str() );
+}
 
 bool GInitContextWindow( const char* title,
 		gContextHandles_t& handles )
 {
 	SDL_Init( SDL_INIT_VIDEO );
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, G_API_MAJOR_VERSION );
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, G_API_MINOR_VERSION );
+	
 	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24 );
+	
+	SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+	SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+	SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+	SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
+
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, G_API_CONTEXT_PROFILE );
 
 	SDL_CreateWindowAndRenderer( handles.width, handles.height, 
@@ -58,4 +89,4 @@ sdl_failure:
 	return false;	
 }
 
-}
+} // end extern "C"
