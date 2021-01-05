79130326dffbd03fa3cacb54a8a144625a2f2c79
Holland Schutte <schutte1@llnl.gov>
2021-01-05 00:21:08 -0800

    fixup gen_workers.sh
    add -lworkerfs.js for web worker file system support, as well as additional exported runtime functions flag.

--- build/gen_workers.sh
+++ build/gen_workers.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Pass -DDEBUG before -I to enable console info 
-
-em++ -I$PWD/../src -fno-inline-functions -O0 -std=c++14 ../src/worker/file_traverse.cxx -s  EXPORTED_FUNCTIONS="['_ReadShaders', '_ReadMapFile_Begin', '_ReadMapFile_Chunk', '_ReadImage', '_MountPackage', '_UnmountPackages']" -s BUILD_AS_WORKER=1 -s TOTAL_MEMORY=234881024 -s STB_IMAGE=1 -o ../worker/file_traverse.js
+# -s "EXPORTED_RUNTIME_METHODS=['stackSave', 'stackRestore', 'dynCall']"
+em++ -I$PWD/../src -fno-inline-functions -O0 -std=c++14 ../src/worker/file_traverse.cxx\
+     -s "EXTRA_EXPORTED_RUNTIME_METHODS=['writeStringToMemory', 'dynCall']"\
+     -s "EXPORTED_FUNCTIONS=['_ReadShaders', '_ReadMapFile_Begin', '_ReadMapFile_Chunk', '_ReadImage', '_MountPackage', '_UnmountPackages']"\
+     -s BUILD_AS_WORKER=1\
+     -s TOTAL_MEMORY=234881024\
+     -s STB_IMAGE=1\
+     -lworkerfs.js -o ../worker/file_traverse.js
